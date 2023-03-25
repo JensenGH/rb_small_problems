4d8935dba1fda6d43b1d59fe1176577d8abaaa78
Jensen Taylor <78d9f80a@slack.launchschool.com>
2023-03-24 19:19:07 -0700

    updated string_assignment problem

--- easy_2/string_assignment.rb
+++ easy_2/string_assignment.rb
@@ -1,42 +1,14 @@
-=begin
-
-Problem:
-- What does this code print out? Think about it for a moment before continuing.
-
-Example code:
 name = 'Bob'
 save_name = name
 name.upcase!
 puts name, save_name
 
-Algo:
-- On line 1, we create a variable called name which holds a string object "Bob".
-- On line 2, we create a second variable called save_name with the same value as the name variable.
-- At this point both these variables are pointing to the same object.
-- On line 3, we invoke the String #upcase! method, which permanently modifies the string into 
-all capital letters.
-- Since both name and save_name are pointing to the same object, if we destructively modify one
-object, the other object also gets modified.
-- Therefore the output for name and save_name, is the names "BOB" printed twice on seperate lines
-since we use 'puts' to output the variables.
+# What does this print out? Explain the results:
 
-=end
-
-# Before mutating the caller
-name = 'Bob'
-p name.object_id
-puts name
-
-save_name = name
-p save_name.object_id
-puts name
-
-name.upcase!
-
-
-# After mutating the caller
-puts name.object_id
-puts name
-
-puts save_name.object_id
-puts save_name
+# When we invoke the Kernel#puts method on line 4 while passing name and save_name as arguments,
+# The string 'BOB' will be output one time on two seperate lines. This is an example of variables
+# as pointers. When we initialize save_name equal to name, these two variables now point to the same
+# string object. When we invoke String#upcase! on line 3, this destruvtive method mutates the object
+# it is called on which is an example of pass by reference. When we pass by reference, we are mutating
+# the calling object rather than reassigning it to a new object. Therefore when we invoke this 
+# method on name, save_name is also mutated.
