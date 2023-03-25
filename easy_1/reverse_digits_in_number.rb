bb8216d42d6576f9fb694e15477b1647998c5cab
Jensen Taylor <78d9f80a@slack.launchschool.com>
2023-03-24 18:05:11 -0700

    Simplified solution to use less methods

--- easy_1/reverse_digits_in_number.rb
+++ easy_1/reverse_digits_in_number.rb
@@ -28,12 +28,11 @@ Algo:
 - Create a method called 'reversed_number'
 - This method takes a positive number as an argument
 - To get the correct output, we can chain a few methods together:
-- First, convert the input into a string using 'to_s'
-- Second, invoke the String #chars method to turn our string of digits into an array of digits.
-- Third, invoke the Array #reverse method to reverse the digits like the problem asks.
-- Lastly, invoke the Array #join method which returns a string created by converting each element
-of the array to a string and call 'to_i' to convert our string back to an integer.
-
+- First we call the Integer#digits method. When calling this method without an arguement,
+will return the given integers in reverse order as an array object.
+- Second, we can call Array#join on the array. When calling this method without an argument
+will return a string by converting each element in the array to a string object.
+- Third, Invoke the String#to_i method to convert the string of numbers back into Integers.
 =end
 
 # Code
