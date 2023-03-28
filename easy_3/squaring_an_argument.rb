f7d6942f838172be9f59ac708553c4971d98204e
Jensen Taylor <78d9f80a@slack.launchschool.com>
2023-03-27 18:06:26 -0700

    Added Further exploration

--- easy_3/squaring_an_argument.rb
+++ easy_3/squaring_an_argument.rb
@@ -18,7 +18,7 @@ Implicit requirements:
 
 Data structure:
 - The input is one integer that could be positive or negative
-- The output is the square the input number
+- The output is the square of the input number
 
 Algo:
 - Create a method named 'square'
@@ -42,3 +42,30 @@ end
 p square(5) == 25
 p square(-8) == 64
 p square(10) == 100
+
+# Further exploration: How could we generalize this method to a 'power to the n' type of method:
+# cubed, to the 4th power, to the 5th, etc. while still using the multiply method?
+
+def cubed(num)
+  multiply(num, num) * num
+end
+
+p cubed(4) == 64
+p cubed(8) == 512
+p cubed(12) == 1728
+
+def fourth_power(num)
+  multiply(num, num) * multiply(num, num)
+end
+
+p fourth_power(4) == 256
+p fourth_power(3) == 81
+p fourth_power(6) == 1296
+
+def fifth_power(num)
+  multiply(num, num) * multiply(num, num) * num
+end
+
+p fifth_power(5) == 3125
+p fifth_power(2) == 32
+p fifth_power(7) == 16807
